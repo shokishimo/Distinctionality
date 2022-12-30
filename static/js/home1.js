574dda14223a261acc17f5f1152accf06fc4110b
shokishimo <shokishimo@gmail.com>
2022-12-30 05:03:57 -0600

    Store given json data into a sessionStorage

--- static/js/home1.js
+++ static/js/home1.js
@@ -2,24 +2,22 @@
 // Get a reference to the button element
 var button = document.getElementById("getData");
 
-var xhr = new XMLHttpRequest();
 
 // Set the calback func to be executed when the button is clicked
 function getData() {
-    // Set the callback func to be executed when the request is done
-    xhr.onload = function() {
-        if (xhr.status == 200) 
-        {
-            // parse json data
-            var data = JSON.parse(xhr.responseText);
-            console.log(data);
-        }
-    };
-
     var level = button.value;
-    var requestStr = "http://localhost:8080/get20Quizzes?version=1&level=" + level
+    var requestStr = '/get20Quizzes?version=1&level=' + level
 
-    xhr.open("GET", requestStr);
+    fetch(requestStr, {method: 'GET'})
+        .then(response => response.json())
+        .then(data => {
+            var count = 1;
+            data.forEach(element => {
+                sessionStorage.setItem('Question'+count, element.Question);
+                sessionStorage.setItem('Answer'+count, element.answer);
+                count++;
+            });
 
-    xhr.send();
+            fetch(requestStr, {method: 'GET'})
+        });
 };
