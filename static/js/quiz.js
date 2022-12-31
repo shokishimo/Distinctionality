97b50b3b624bd4023484d08833f1afb8d153606a
shokishimo <shokishimo@gmail.com>
2022-12-31 04:23:30 -0600

    Implemented a functionality that let users flip the text to see answer/question

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -1,15 +1,41 @@
 var questionNo = 1;
+var answerOrQuestion = true; // true == answer, false = question to show on button
 
-var questionNoArea = document.querySelector('.No');
-var question = document.getElementById('qanda');
+var questionNoArea;
+var question;
+var leftBtn;
+var rightBtn;
+var midBtn;
 
 window.onload = function() {
+    questionNoArea = document.querySelector('.No');
+    question = document.getElementById('qanda');
+    leftBtn = document.querySelector('.left');
+    rightBtn = document.querySelector('.right');
+    midBtn = document.querySelector('.flip');
+
     // question number
     questionNoArea.innerHTML = 'Q' + questionNo;
-
     // get question from sessionStorage
     var qData = sessionStorage.getItem('Question'+questionNo);
-
     // Question text
     question.innerHTML = qData;
+
+    midBtn.innerHTML = ' answer ';
+};
+
+function flip() {
+    var valueToShow;
+
+    if (answerOrQuestion) {
+        valueToShow = sessionStorage.getItem('Answer'+questionNo);
+        question.innerHTML = valueToShow;
+        midBtn.innerHTML = "question"
+        answerOrQuestion = false;
+    } else {
+        valueToShow = sessionStorage.getItem('Question'+questionNo);
+        question.innerHTML = valueToShow;
+        midBtn.innerHTML = " answer "
+        answerOrQuestion = true;
+    }
 };
