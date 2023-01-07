65968e8271fb9f800589e3616447b41170d69c14
shokishimo <shokishimo@gmail.com>
2023-01-07 06:18:49 -0600

    Previous change creates error in connecting db so unmodified the previous change

--- db/db.go
+++ db/db.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"os"
 
-	// "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -14,10 +14,10 @@ import (
 func Connect() (*mongo.Client, error) {
 	var client *mongo.Client
 	var uri string
-	// if err := godotenv.Load(); err != nil {
-	// 	log.Println("No .env file found")
-	// 	return client, err
-	// }
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+		return client, err
+	}
 	uri = os.Getenv("MONGODB_URI")
 	if uri == "" {
 		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
